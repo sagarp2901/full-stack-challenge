359c094b09d1678a9d35232f84b7937a09a77c0c
Sagar <sagar.patel166@gmail.com>
2018-12-02 14:22:55 -0500

    Add update delete employee with new layout

--- performance-review-app/src/app/components/employee/employee.component.ts
+++ performance-review-app/src/app/components/employee/employee.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { Employee } from "src/app/models/employee";
 import { AdminService } from "src/app/services/admin.service";
+import { MatDialog } from "@angular/material";
+import { AddEmployeeDialogComponent } from "src/app/components/add-employee-dialog/add-employee-dialog.component";
 
 @Component({
   selector: "app-employee",
@@ -10,16 +12,33 @@ import { AdminService } from "src/app/services/admin.service";
 export class EmployeeComponent implements OnInit {
   @Input() employee: Employee;
 
-  @Output() employeeDeleted = new EventEmitter();
+  @Output() employeeUpdated = new EventEmitter();
 
-  constructor(private adminService: AdminService) {}
+  employees: Employee[];
+
+  constructor(private adminService: AdminService, public dialog: MatDialog) {}
 
   ngOnInit() {}
 
   deleteEmployee(employeeId) {
     this.adminService.deleteEmployee(employeeId).subscribe((data: any) => {
       console.log("Deleted successfully");
-      this.employeeDeleted.emit(data.employees);
+      this.employeeUpdated.emit(data.employees);
+    });
+  }
+
+  openDialog(mode, employee): void {
+    const dialogRef = this.dialog.open(AddEmployeeDialogComponent, {
+      data: { mode, employee },
+      width: "250px"
+    });
+
+    dialogRef.componentInstance.updateAdminView.subscribe(data => {
+      this.employeeUpdated.emit(data.employees);
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log("The dialog was closed");
     });
   }
 }

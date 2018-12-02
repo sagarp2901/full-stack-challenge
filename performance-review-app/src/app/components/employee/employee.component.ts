6a8fd0bc1328f7789fa25dfe8da350ce8557437a
Sagar <sagar.patel166@gmail.com>
2018-12-02 17:49:58 -0500

    Added colors card and image

--- performance-review-app/src/app/components/employee/employee.component.ts
+++ performance-review-app/src/app/components/employee/employee.component.ts
@@ -10,6 +10,26 @@ import { AddEmployeeDialogComponent } from "src/app/components/add-employee-dial
   styleUrls: ["./employee.component.scss"]
 })
 export class EmployeeComponent implements OnInit {
+  colors = [
+    "#FF6969",
+    "#69FF69",
+    "#6969FF",
+    "#FFFF5D",
+    "#69FFFF",
+    "#FF69FF",
+    "#CDB075",
+    "#FFAFAF",
+    "#AFFFAF",
+    "#AFAFFF",
+    "#FFFFAF",
+    "#AFFFFF",
+    "#FFAFFF",
+    "#E4D5B5",
+    "#A4B086",
+    "#819EC1"
+  ];
+
+  @Input() colorIndex = 0;
   @Input() employee: Employee;
 
   @Output() employeeUpdated = new EventEmitter();
@@ -19,6 +39,12 @@ export class EmployeeComponent implements OnInit {
   constructor(private adminService: AdminService, public dialog: MatDialog) {}
 
   ngOnInit() {}
+
+  renderColor() {
+    return this.colors[this.colorIndex]
+      ? this.colors[this.colorIndex]
+      : this.colors[this.colorIndex % 2];
+  }
 
   updateReview(review, employee) {
     review.isEdit = false;

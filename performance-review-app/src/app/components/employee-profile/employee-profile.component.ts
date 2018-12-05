12605fa41eb2e415328fb6e9c24d6df132123bfa
Sagar <sagar.patel166@gmail.com>
2018-12-04 23:22:55 -0500

    Added comments and improvised the code.

--- performance-review-app/src/app/components/employee-profile/employee-profile.component.ts
+++ performance-review-app/src/app/components/employee-profile/employee-profile.component.ts
@@ -22,14 +22,18 @@ export class EmployeeProfileComponent implements OnInit {
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.id = params["id"];
+      // Get current logged in employee
       this.adminService.getEmployeeById(this.id).subscribe(data => {
         this.currentEmployee = data;
+        // Get list of all the employees to find out which employee needs feedback from the current employee
         this.adminService.getEmployees().subscribe((data: []) => {
           this.employees = data;
 
+          // Creating a list of employees that require review from this current employee
           this.employees.forEach((emp: any) => {
             emp.reviewers.forEach((rev: any) => {
               if (rev.name == this.currentEmployee.name) {
+                if (!emp.feedback) emp.feedback = { text: "", isSaved: false };
                 this.writeReviewFor.push(emp);
               }
             });
@@ -40,23 +44,26 @@ export class EmployeeProfileComponent implements OnInit {
   }
 
   submitFeedback(employee, isComplete) {
+    // Adding feedback for the employee that needs feedback from this current employee
     employee.feedbacks.push({
-      text: this.employeeFeedback,
+      text: employee.feedback.text,
       isComplete,
       feedbackId: this.currentEmployee._id,
       feedBackBy: this.currentEmployee.name
     });
-    this.employeeFeedback = "";
 
-    // Remove the completed employee from the view
-    this.writeReviewFor = this.writeReviewFor.filter(emp => {
-      return emp._id !== employee._id;
-    });
+    if (isComplete) {
+      // this.employeeFeedback = "";
+      // Remove the employee with completed review from the view
+      this.writeReviewFor = this.writeReviewFor.filter(emp => {
+        return emp._id !== employee._id;
+      });
 
-    // Remove the reviewer from this person's database entry
-    employee.reviewers = employee.reviewers.filter(rev => {
-      return rev._id !== this.currentEmployee._id;
-    });
+      // Remove the reviewer from this person's database entry whose review is complete
+      employee.reviewers = employee.reviewers.filter(rev => {
+        return rev._id !== this.currentEmployee._id;
+      });
+    }
 
     this.adminService.updateEmployee(employee).subscribe(data => {
       console.log("Feedback Saved successfully");

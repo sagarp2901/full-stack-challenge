852a69a8eaf1d2d86ece863ae3a3a2c3693d8f63
Sagar <sagar.patel166@gmail.com>
2018-12-05 23:00:03 -0500

    Removed unused components

--- performance-review-app/src/app/app-routing.module.ts
+++ performance-review-app/src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { HomeComponent } from "src/app/components/home/home.component";
-import { EmployeeViewComponent } from "src/app/components/employee-view/employee-view.component";
 import { EmployeeProfileComponent } from "src/app/components/employee-profile/employee-profile.component";
 import { AdminComponent } from "./components/admin/admin.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "/admin", pathMatch: "full" },
-  { path: "home", component: HomeComponent },
   { path: "admin", component: AdminComponent },
-  { path: "employee", component: EmployeeViewComponent },
   { path: "employee-profile/:id", component: EmployeeProfileComponent }
 ];
 
--- performance-review-app/src/app/app.module.ts
+++ performance-review-app/src/app/app.module.ts
@@ -21,8 +21,7 @@ import {
   MatSelectModule,
   MatMenuModule
 } from "@angular/material";
-import { HomeComponent } from "./components/home/home.component";
-import { EmployeeViewComponent } from "./components/employee-view/employee-view.component";
+
 import { EmployeeProfileComponent } from "./components/employee-profile/employee-profile.component";
 import { AddEmployeeDialogComponent } from "./components/add-employee-dialog/add-employee-dialog.component";
 import { AdminComponent } from "./components/admin/admin.component";
@@ -31,8 +30,6 @@ import { EmployeeComponent } from "./components/employee/employee.component";
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    EmployeeViewComponent,
     EmployeeProfileComponent,
     AddEmployeeDialogComponent,
     AdminComponent,
